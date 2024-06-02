2700692683eee07d211098c20f38b62e040bb90f
sahil <sahil27raj@gmail.com>
2024-06-02 16:32:21 +0530

    create the update user api

--- api/index.js
+++ api/index.js
@@ -4,12 +4,14 @@ import { configDotenv } from "dotenv";
 import userRouter from './routes/user.route.js';
 import authRouter from './routes/auth.route.js';
 import connectDB from './utils/db.js'
+import cookieParser from "cookie-parser";
 
 configDotenv();
 const app = express();
 
 connectDB();
 app.use(express.json())
+app.use(cookieParser());
 
 app.use('/api/user',userRouter);
 app.use('/api/auth',authRouter);
