0ecf0e04079372bda46e2c54dfd6faaa1c3f3102
sahil <sahil27raj@gmail.com>
2024-07-07 15:35:20 +0530

    complete the home and about page

--- client/src/pages/About.jsx
+++ client/src/pages/About.jsx
@@ -1,9 +1,33 @@
 import React from 'react'
 
-const About = () => {
+export default function About() {
   return (
-    <div>About</div>
+    <div className='py-20 px-4 max-w-6xl mx-auto'>
+  <h1 className='text-3xl font-bold mb-4 text-slate-800'>About Sahil Estate</h1>
+  <p className='mb-4 text-slate-700'>
+    At Sahil Estate, we are more than just a real estate agency; we are your trusted partners in finding your dream home or the perfect investment property. Specializing in the most sought-after neighborhoods, our team is committed to delivering an unparalleled experience in buying, selling, and renting properties.
+  </p>
+  <p className='mb-4 text-slate-700'>
+    Our mission is to turn your real estate aspirations into reality by offering expert guidance, personalized service, and an in-depth understanding of the local market dynamics. Whether you're a first-time homebuyer, a seasoned investor, or looking to rent, we are here to support you at every stage of your journey.
+  </p>
+  <p className='mb-4 text-slate-700'>
+    <strong>Why Choose Us?</strong>
+  </p>
+  <p className='mb-4 text-slate-700'>
+    <ul className='list-disc pl-5'>
+      <li><strong>Expert Agents:</strong> Our team comprises highly experienced and knowledgeable agents who are passionate about real estate. Their expertise ensures that you receive the best advice and the most current market insights.</li>
+      <li><strong>Personalized Service:</strong> We understand that every client has unique needs and preferences. Our personalized approach means we tailor our services to meet your specific requirements.</li>
+      <li><strong>Smooth Transactions:</strong> We aim to make the buying, selling, or renting process as seamless and stress-free as possible. From the initial consultation to closing, we handle all the details with professionalism and care.</li>
+      <li><strong>Client-Centric Approach:</strong> At Sahil Estate, your satisfaction is our top priority. We believe that real estate transactions should be exciting and rewarding, and we strive to create a positive experience for each of our clients.</li>
+    </ul>
+  </p>
+  <p className='mb-4 text-slate-700'>
+    Discover the difference with Sahil Estate. Let us help you navigate the real estate market with confidence and ease. Whether you're looking to find your next home or make a smart investment, our dedicated team is here to make your real estate dreams come true.
+  </p>
+  <p className='mb-4 text-slate-700'>
+    Reach out to Sahil Estate and start your journey towards achieving your real estate goals. We are excited to work with you and provide the exceptional service you deserve.
+  </p>
+</div>
+
   )
 }
-
-export default About
